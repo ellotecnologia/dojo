a983cc148fe86546e2a4770cc2c9740c3cc11a54
Láyro Cristofér <layrozahaila@gmail.com>
2019-03-09 00:50:23 -0400

    Resolução do desafio do PDV em PHP

---
+++ php/pdv.php
@@ -0,0 +1,31 @@
+<?php declare (strict_types=1);
+
+//PHP7.2
+
+/*
+ * Calcula quantidade de moedas de diversos valores para troco
+ *
+ * @param float $valorTotal
+ * @param float $valorPago
+ * @return string
+ */
+function calculaTroco(float $valorTotal, float $valorPago)
+{
+    $troco          = $valorPago - $valorTotal;
+    if ($troco <= 0) return "Sem troco";
+
+    $moedas     = ['1.00' => 0, '0.25' => 0, '0.05' => 0, '0.01' => 0];
+    $resultado  = '';
+    foreach ($moedas as $moeda => $qtde) {
+        while ($troco >= $moeda) {
+            $moedas[$moeda]++;
+            $troco -= $moeda;
+        }
+        if ($moedas[$moeda] > 0) {
+            $resultado .= "\n{$moedas[$moeda]} moeda(s) de \${$moeda}";
+        }
+    }
+    return $resultado;
+}
+fscanf(STDIN, "%f%f", $valorTotal, $valorPago);
+printf("%s\n", calculaTroco($valorTotal, $valorPago));
